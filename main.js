118b3038cfab3aad4aaa92029112e47603517965
Jacob <jakeboone@aol.com>
2014-06-30 14:05:40 -0600

    4 progress

--- main.js
+++ main.js
@@ -4,29 +4,40 @@ $(document).on('ready', function() {
 // EXAMPLE
 
 var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
-	for(var i=0; i<animals.length; i++) {
- 	console.log(animals[i]);
-	}
+	// 	for(var i=0; i<animals.length; i++) {
+	//  	console.log(animals[i]);
+	// 	}
 
-console.log('---------problem 1 ---------')
-	for(var i=0; i<animals.length -1; i++) {
- 	console.log(animals[i]);
-	}
+	// console.log('---------problem 1 ---------')
+	// 	for(var i=0; i<animals.length -1; i++) {
+	//  	console.log(animals[i]);
+	// 	}
 
-console.log('---------problem 2 ---------')
-	for(var i=0; i<animals.length; i+=2) {
- 	console.log(animals[i]);
-	}
-
-
-
-console.log('---------problem 3 ---------')
-	for(var i=animals.length -1; i>=0; i--) {
- 	console.log(animals[i]);
-	}
+	// console.log('---------problem 2 ---------')
+	// 	for(var i=0; i<animals.length; i+=2) {
+	//  	console.log(animals[i]);
+	// 	}
 
 
 
+	// console.log('---------problem 3 ---------')
+	// 	for(var i=animals.length -1; i>=0; i--) {
+	//  	console.log(animals[i]);
+	// 	}
+
+
+
+console.log('---------problem 4 ---------')
+	for(var i=1; i<animals.length-1; i++){
+		console.log(animals[i]); 
+
+		for(var i=0; i<animals.length; i++) {
+	console.log(animals[i]);
+
+	
+}			
+
+ 
 
 
 
@@ -34,4 +45,7 @@ console.log('---------problem 3 ---------')
 
 
 
-});
+
+
+
+// });
